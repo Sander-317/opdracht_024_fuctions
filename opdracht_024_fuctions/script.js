63f5492bce48f0cf7bef06f5909785421dde93f4
Sander-317 <80258328+Sander-317@users.noreply.github.com>
2021-04-09 13:32:21 +0200

    Add files via upload

---
+++ opdracht_024_fuctions/script.js
@@ -0,0 +1,21 @@
+
+const bakePizza = function () {
+    console.log("make do");
+    console.log("make pizza bottom");
+    console.log("put topping on the pizza");
+
+}
+const makeSusi = function () {
+    console.log("catch fish");
+    console.log("get sea weed");
+    console.log("get rice");
+    console.log("roll the susi");
+
+}
+
+
+makeSusi();
+makeSusi();
+makeSusi();
+bakePizza();
+bakePizza();
